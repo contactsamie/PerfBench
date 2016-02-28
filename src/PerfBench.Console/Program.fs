8effe8aac2c97247efbca69ba488e0173679c163
Vlad Kosarev <vlad@webmustflow.com>
2016-02-28 17:24:38 -0500

    working initial skeleton

---
+++ src/PerfBench.Console/Program.fs
@@ -0,0 +1,32 @@
+﻿open PerfBench.Engine;
+open System
+open HttpClient
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+[<EntryPoint>]
+let main argv =
+    let getRandomNumberAsString max =
+        let rnd = System.Random()
+        string (rnd.Next(max))
+
+    let GetUserByPost post = async {        
+        let baseUrl = "https://httpbin.org/get"
+        printfn "response"
+        let! response = createRequest Get baseUrl |> getResponseAsync
+        printfn "got response"
+        match response.StatusCode with
+            | 200 -> 
+                let parsedResponse = JsonValue.Parse(response.EntityBody.Value)
+                let userId = parsedResponse?origin.AsString()
+                printfn "%s\r\n" userId
+            | _ -> 
+                printfn "Failed with code: %s" (string response.StatusCode)
+        ()
+        }
+
+    let l = runTest "user" 10 (fun() -> GetUserByPost "1")
+
+    System.Console.ReadLine() |> ignore
+
+    0
---
+++ src/PerfBench/PerfBench.fs
@@ -0,0 +1,55 @@
+﻿module PerfBench.Engine
+
+open Akka.FSharp
+open Akka.Configuration
+open System
+open HttpClient
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+// initialize
+let private config = @"
+    akka {
+        log-config-on-start = on
+        stdout-loglevel = DEBUG
+        loglevel = DEBUG
+        # this config section will be referenced as akka.actor
+        actor { 
+          debug {
+              receive = on
+              autoreceive = on
+              lifecycle = on
+              event-stream = on
+              unhandled = on
+          }
+        }
+    }"          
+let private config2 = @"";
+
+let private system = System.create "MySystem" <| Configuration.parse(config2)
+
+//
+type private Messages = Execute | Other
+
+let private handleMessage msg func =
+        match msg with
+            | Execute -> Async.Start (func())
+            | _ -> ()
+
+let private funcWithLogging name func = async {
+    let timer = new System.Diagnostics.Stopwatch()
+    timer.Start()
+    printfn "Started %s" name
+    let! result = func()
+    printfn "Elapsed Time: %f" timer.Elapsed.TotalSeconds
+    printfn "Finished %s" name
+    result}
+
+let runTest namePrefix numUsers func =
+    [0..numUsers] 
+        |> List.map (fun i ->         
+            let name = namePrefix + (string i)
+            let newFunc = fun() -> funcWithLogging name func
+            let ref = spawn system name (actorOf (fun msg -> handleMessage msg newFunc)) <! Execute
+            ref)
+
