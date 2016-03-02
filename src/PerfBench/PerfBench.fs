dc06376c7abcb1f93a71f8eeaf5251e10149e663
Vlad Kosarev <vlad@webmustflow.com>
2016-03-02 11:43:06 -0500

    A really nasty way of pushing real-time data to the client. Will figure out a nicer way later.

--- src/PerfBench/PerfBench.fs
+++ src/PerfBench/PerfBench.fs
@@ -7,9 +7,14 @@ open System.Text
 open System.Reflection
 open System.Resources
 open System.IO
+open System.Linq
 open HttpClient
 open FSharp.Data
 open FSharp.Data.JsonExtensions
+open Newtonsoft.Json
+
+open Nessos.FsPickler
+open Nessos.FsPickler.Json
 
 open Suave
 open Suave.Http
@@ -24,8 +29,6 @@ open Suave.Utils
 open Suave.Sockets
 open Suave.Sockets.Control
 open Suave.WebSocket
-
-
 
 // initialize akka
 let private config = @"
@@ -52,6 +55,7 @@ let private system = System.create "MySystem" <| Configuration.parse(config2)
 type private Messages = Execute
 type private CoordinatorMessages = Create | Finished of string*float | Stats
 type private TaskStatus = Executing | Succeeded of float | Failed of string
+type private Events = FinishedEvent of string*float
 
 let private funcWithLogging name func parent = async {
   let timer = new System.Diagnostics.Stopwatch()
@@ -64,11 +68,59 @@ let private funcWithLogging name func parent = async {
 let runTest namePrefix numUsers func =
   let coordinatorRef =
     spawn system "Coordinator" <| fun mailbox ->
+      let event = new Event<'t>()
+      let publishedEvent = event.Publish
+      let events = ref List.empty
+      do publishedEvent |> Observable.subscribe (fun x -> events := ([x] :: !events))
       let rec loop state =
         actor {
           let! msg = mailbox.Receive()                    
           match msg with
-            | Create -> return! loop ([1..numUsers] 
+            | Create -> 
+              let webServerRef = spawn system "webServer" <| fun mailbox ->
+                let echo (webSocket : WebSocket) =
+                  fun cx -> socket {
+                    do publishedEvent |> Observable.subscribe (fun x -> 
+                                                                let jsonSerializer = FsPickler.CreateJsonSerializer(indent = true)
+                                                                let str = Encoding.UTF8.GetBytes(jsonSerializer.PickleToString (x))
+                                                                do webSocket.send Text str true |> Async.StartAsTask                                                                
+                                                                ) |> ignore                    
+                    let loop = ref true
+                    while !loop do
+                      let! msg = webSocket.read()
+                      match msg with
+                      | (Text, data, true) ->                        
+                        let jsonSerializer = FsPickler.CreateJsonSerializer(indent = true)
+                        let str = Encoding.UTF8.GetBytes(jsonSerializer.PickleToString (events))
+                        do! webSocket.send Text str true
+                      | (Ping, _, _) ->
+                        do! webSocket.send Pong [||] true
+                      | (Close, _, _) ->
+                        do! webSocket.send Close [||] true
+                        loop := false
+                      | _ -> ()
+                  }
+                
+                let executingAssembly = Assembly.GetExecutingAssembly()
+                let sr = new StreamReader(executingAssembly.GetManifestResourceStream("index.html"));
+                let index = sr.ReadToEnd()
+                
+                let app : WebPart =
+                  choose [
+                    path "/websocket" >=> handShake echo
+                    //GET >=> choose [ path "/" >=> file "index.html"; browseHome ];
+                    GET >=> choose [ path "/" >=> OK index ];
+                    NOT_FOUND "Found no handlers."
+                    ]
+  
+                do startWebServer { defaultConfig with logger = Loggers.ConsoleWindowLogger LogLevel.Warn } app
+                let rec loop() =
+                    actor { 
+                        let! msg = mailbox.Receive()
+                        return! loop()                                                                                            
+                    }
+                loop()
+              return! loop ([1..numUsers] 
                 |> List.map (fun i ->         
                   let name = namePrefix + (string i)                                                                                                                        
                   let newFunc = fun() -> funcWithLogging name func mailbox.Self
@@ -77,26 +129,28 @@ let runTest namePrefix numUsers func =
                         actor { 
                             let! msg = mailbox.Receive()
                             match msg with
-                                | Execute -> Async.Start (newFunc())                                                                                                    
+                              | Execute -> Async.Start (newFunc())                                                                                                    
                         }
                     loop []
                   do ref <! Execute                                                            
                   name,(ref,Executing)))
             | Finished (name, time) -> 
                 let newState = (state
-                  |> List.map (fun elem ->
+                    |> List.map (fun elem ->
                     match elem with
                       | n,(ref,Executing) when n = name -> 
                           //printfn "Elapsed Time for %s is %f" name time
                           printf "."
                           n,(ref,Succeeded time)
                       | n,(ref,status) -> n,(ref,status)
-                  ))
+                    ))
                 let leftToProcess = newState |> List.filter (fun e -> 
                                                             let n,(r,s) = e
                                                             s = Executing)
                                              |> List.length
                 do if (leftToProcess = 0) then mailbox.Self <! Stats
+                do event.Trigger(FinishedEvent (name,time))
+                //events := [FinishedEvent (name,time)] :: !events
                 return! loop newState
             | Stats -> 
                 let average = state |> List.averageBy (fun e -> 
@@ -113,47 +167,9 @@ let runTest namePrefix numUsers func =
                 printfn "Done processing batch."
                 printfn "Average processing time is %f" average
                 printfn "Max processing time is %f" max
+                //mystate := state
                 return! loop state
         }
       loop []
   do coordinatorRef <! Create
-  
-  let webServer = spawn system "webServer" <| fun mailbox ->    
-
-    let echo (webSocket : WebSocket) =
-      fun cx -> socket {
-        let loop = ref true
-        while !loop do
-          let! msg = webSocket.read()
-          match msg with
-          | (Text, data, true) ->
-            let str = Encoding.UTF8.GetString data
-            do! webSocket.send Text data true
-          | (Ping, _, _) ->
-            do! webSocket.send Pong [||] true
-          | (Close, _, _) ->
-            do! webSocket.send Close [||] true
-            loop := false
-          | _ -> ()
-      }
-    
-    let executingAssembly = Assembly.GetExecutingAssembly()
-    let sr = new StreamReader(executingAssembly.GetManifestResourceStream("index.html"));
-    let index = sr.ReadToEnd()
-    
-    let app : WebPart =
-      choose [
-        path "/websocket" >=> handShake echo
-        //GET >=> choose [ path "/" >=> file "index.html"; browseHome ];
-        GET >=> choose [ path "/" >=> OK index ];
-        NOT_FOUND "Found no handlers."
-        ]
-  
-    do startWebServer { defaultConfig with logger = Loggers.ConsoleWindowLogger LogLevel.Verbose } app
-    let rec loop() =
-        actor { 
-            let! msg = mailbox.Receive()
-            return! loop()                                                                                            
-        }
-    loop()
   ()    
